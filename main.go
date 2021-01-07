7912a2c793bfa1cc89717b203b4645ac700ba95f
nicholasvuono <nicholas.vuono@gmail.com>
2021-01-07 01:25:53 -0500

    added additional logic to cli

--- main.go
+++ main.go
@@ -3,10 +3,25 @@ package main
 import (
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/urfave/cli"
 )
 
 func main() {
-	log.Fatal(cli.NewApp().Run(os.Args))
+	app := cli.NewApp()
+	app.Name = "Sense JMeter Agent"
+	app.Usage = "This is a CLI built using Go to allow users to run a JMeter test and send the results to an implemetation of the sense-microservice to be formatted and stored!"
+	app.Version = "1.0.0"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "run",
+			Usage: "Runs the JMeter test (.jmx) that is specified",
+			Action: func(c *cli.Context) error {
+				_, err := exec.Command("")
+			},
+		},
+	},
+		log.Fatal(app.Run(os.Args))
 }
