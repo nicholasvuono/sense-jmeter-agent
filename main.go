0f62c47b5548bb41f19f491a0a6b048132d7ad33
nicholasvuono <nicholas.vuono@gmail.com>
2021-01-07 22:33:39 -0500

    Added run command
    Added logic for the run command that will run the jmeter test and send the json formatted result ot the specified url of the sense microservice instance. The run command accepts the jmeter executable flag -exec, the jmeter test flag -jmx, the jmeter result flag -jtl, the sense microservice url -url, and the test type flag (plu/blu) -type.

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
+
+	jtltojson "github.com/nicholasvuono/jtl-to-json"
 
 	"github.com/urfave/cli"
 )
@@ -14,14 +18,32 @@ func main() {
 	app.Usage = "This is a CLI built using Go to allow users to run a JMeter test and send the results to an implemetation of the sense-microservice to be formatted and stored!"
 	app.Version = "1.0.0"
 
-	app.Commands = []cli.Command{
+	app.Commands = []*cli.Command{
 		{
 			Name:  "run",
 			Usage: "Runs the JMeter test (.jmx) that is specified",
 			Action: func(c *cli.Context) error {
-				_, err := exec.Command("")
+				err := exec.Command(
+					c.String("exec"),
+					"-n",
+					"-t "+c.String("jmx"),
+					"-l "+c.String("jtl"),
+				).Run()
+				if err != nil {
+					return err
+				}
+				_, err = http.Post(
+					c.String("url")+"/results/"+c.String("type")+"/add",
+					"application/json",
+					bytes.NewBuffer(jtltojson.Ptor(c.String("jtl")).JSON()),
+				)
+				if err != nil {
+					return err
+				}
+				return nil
 			},
 		},
-	},
-		log.Fatal(app.Run(os.Args))
+	}
+
+	log.Fatal(app.Run(os.Args))
 }
